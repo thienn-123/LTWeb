de6f8b7ec5d26a77d6bfd8bed0fc3de44271c8d0
Your Name <you@example.com>
2024-09-11 13:51:07 +0700

    Hoan thanh bai tap

---
+++ BaiTap06/Controllers/TaiKhoanControllercs.cs
@@ -0,0 +1,21 @@
+﻿using BaiTap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap06.Controllers
+{
+    public class TaiKhoanControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.Password != null && taikhoan.Password.Length > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_da_ma_hoa";
+            }   
+            return View(taikhoan);
+        }
+    }
+}
